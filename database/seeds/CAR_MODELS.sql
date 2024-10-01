de9f432d4e0e2c463944ff9d6536bdaa1dc097e6
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-01 12:39:39 -0300

    feat: cria seeds para models

---
+++ database/seeds/CAR_MODELS.sql
@@ -0,0 +1,17 @@
+-- Dados
+
+INSERT INTO CAR_MODELS (MODEL_NAME)
+VALUES
+('Conversível'),
+('Sedã'),
+('Hatch'),
+('Coupé'),
+('Perua'),
+('SUV'),
+('Picape'),
+('Minivan'),
+('Utilitário'),
+('Buggy');
+
+SELECT * FROM CAR_MODELS;
+
