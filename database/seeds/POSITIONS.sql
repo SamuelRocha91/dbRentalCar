0f6db0cffcddfc0ea68c7f22488b91390f958011
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-01 12:42:53 -0300

    feat: cria seeds para positions

---
+++ database/seeds/POSITIONS.sql
@@ -0,0 +1,11 @@
+-- Dados
+
+INSERT INTO POSITIONS (ID, DESCRIPTION)
+VALUES
+(1, 'Gerente de vendas'),
+(2, 'Gerente de compras'),
+(3, 'Vendedor'),
+(4, 'Mecânico'),
+(5, 'Assistente Administrativo');
+
+SELECT * FROM POSITIONS;
