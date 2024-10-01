8f5abb023911421f7b0bfa306114d8f5a84b919f
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-01 12:41:40 -0300

    feat: cria seeds para employees

---
+++ database/seeds/EMPLOYEES.sql
@@ -0,0 +1,12 @@
+-- Dados
+
+INSERT INTO EMPLOYEES (ID, NAME, PHONE, CONTRACT_DATE, POSITION_ID)
+VALUES
+(1, 'Adriana Lemes', '(11) 98789-9999', '2021-04-10', 1),
+(2, 'Camila Soares', '(11) 92749-9599', '2008-03-25', 3),
+(3, 'Leonardo Silva', '(11) 91449-9600', '2018-08-26', 5),
+(4, 'Mayara Gomes', '(11) 92649-9601', '2016-07-18', 2),
+(5, 'Matheus Alves', '(11) 92749-9602', '2015-03-04', 5),
+(6, 'Aline Santos', '(11) 99789-3135', '2020-05-06', 3);
+
+SELECT * FROM EMPLOYEES;
