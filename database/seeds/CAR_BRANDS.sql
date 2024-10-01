8bf4cfdc819427edba14207ba243f5e706b4be00
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-01 12:39:17 -0300

    feat: cria seeds para brands

---
+++ database/seeds/CAR_BRANDS.sql
@@ -0,0 +1,14 @@
+-- Dados
+
+INSERT INTO CAR_BRANDS (ID, BRAND_NAME)
+VALUES
+(1, 'Chevrolet'),
+(2, 'Toyota'),
+(3, 'Hyundai'),
+(4, 'Volkswagen'),
+(5, 'Jeep'),
+(6, 'Renault'),
+(7, 'Honda'),
+(8, 'Fiat');
+
+SELECT * FROM CAR_BRANDS;
